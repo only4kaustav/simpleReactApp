c4b7d7c8e704988f4da6814eea23be57aa710cf8
Kaustav Ghosh <kghosh8@its.jnj.com>
2019-10-27 18:11:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            My First React App using nodejs.
+            My First React App using nodejs modified.
           </p>
           <a
             className="App-link"
